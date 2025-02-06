ca8cf6940da583894ce188754fcd465b337a6d0c
pradhyuman-yadav <pradhyuman999@gmail.com>
2025-02-05 20:01:36 -0500

    Fixed the build issue

--- app/(withbar)/posts/[slug]/page.tsx
+++ app/(withbar)/posts/[slug]/page.tsx
@@ -2,7 +2,7 @@ import fs from "fs";
 import Markdown from "markdown-to-jsx";
 import matter from "gray-matter";
 import getPostMetadata from "@/components/getPostMetadata";
-import { DotFilledIcon } from "@radix-ui/react-icons";
+import { DotFilledIcon } from "@radix-ui/react-icons"
 
 const getPostContent = (slug: string) => {
   const folder = "posts/";
@@ -19,26 +19,22 @@ export const generateStaticParams = async () => {
   }));
 };
 
-export default async function PostPage(props: {
-  params: Promise<{ slug: string }>;
-}) {
+export default async function PostPage(props: {params: Promise<{ slug: string }>}) {
   const params = await props.params;
   const { slug } = params;
   const post = getPostContent(slug);
 
   return (
-    <div className="mx-auto p-6">
+    <div className="max-w-3xl mx-auto p-6">
       {/* Post Title and Date */}
       <div className="my-12 flex items-center space-x-2">
-        <h1 className="text-3xl font-bold text-foreground">
-          {post.data.title}
-        </h1>
-        <DotFilledIcon className="text-muted-foreground" />
-        <p className="text-sm text-muted-foreground">{post.data.date}</p>
+        <h1 className="text-3xl font-bold text-foreground">{post.data.title}</h1>
+        <DotFilledIcon className="text-border" />
+        <p className="text-sm text-secondary-foreground">{post.data.date}</p>
       </div>
 
       {/* Markdown Content */}
-      <article className="prose dark:prose-invert max-w-none">
+      <article className="prose dark:prose-invert text-foreground">
         <Markdown>{post.content}</Markdown>
       </article>
     </div>
